d5c47f1ce17f4ed2537cfff8e6deab18ee5fbb57
cstria0106 <cstria0106@users.noreply.github.com>
2023-11-26 12:46:25 +0000

    deploy: f6e27f0c99709714a144036377a2f3c2957e6eda

---
+++ _app/immutable/chunks/app.c281b75d.js
@@ -0,0 +1,1 @@
+import{b as o}from"./paths.372eef51.js";function r(){location.assign(`${o}/`)}export{r};
--- _app/immutable/chunks/app.d976be1e.js
+++
@@ -1,1 +0,0 @@
-import{b as o}from"./paths.8b72ff1a.js";function r(){location.assign(`${o}/`)}export{r};
