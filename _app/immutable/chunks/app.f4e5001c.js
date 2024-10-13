7599a7cd063478c9a0ad58e1ed3f25ed107c1eac
cstria0106 <cstria0106@users.noreply.github.com>
2024-10-13 04:01:25 +0000

    deploy: fc2f94c3e14d3b853590db1bc6639793bc7d7d7d

--- _app/immutable/chunks/app.f4e5001c.js
+++
@@ -1,1 +0,0 @@
-import{b as o}from"./paths.4fa77d1b.js";function r(){location.assign(`${o}/`)}export{r};
---
+++ _app/immutable/chunks/navigation.095eed3c.js
@@ -0,0 +1,1 @@
+import{h as o}from"./singletons.c4994a9f.js";const e=o("goto");export{e as g};
